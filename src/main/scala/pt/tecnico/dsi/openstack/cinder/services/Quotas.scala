9a6b47db96e9dfccb55f687efd3bf6b3af604e57
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2020-12-10 13:09:51 +0000

    Update dependencies.
    Small improvements.

--- src/main/scala/pt/tecnico/dsi/openstack/cinder/services/Quotas.scala
+++ src/main/scala/pt/tecnico/dsi/openstack/cinder/services/Quotas.scala
@@ -8,39 +8,39 @@ import pt.tecnico.dsi.openstack.cinder.models.{Quota, QuotaUsage}
 import pt.tecnico.dsi.openstack.common.services.Service
 import pt.tecnico.dsi.openstack.keystone.models.Session
 
-final class Quotas[F[_]: Sync: Client](baseUri: Uri, session: Session) extends Service[F](session.authToken) {
-  val uri: Uri = baseUri / "os-quota-sets"
-  val name = "quota_set"
-
+final class Quotas[F[_]: Sync: Client](baseUri: Uri, session: Session) extends Service[F](baseUri, "quota_set", session.authToken) {
+  override val uri: Uri = baseUri / "os-quota-sets"
+  
+  private val wrappedAt: Option[String] = Some(name)
+  
   /**
     * Shows quotas for a project.
     * Cinder always returns a Quota even if the project does not exist. That is why there is no method called `get`.
     * @param projectId The UUID of the project.
     */
-  def apply(projectId: String): F[Quota] = super.get(wrappedAt = Some(name), uri / projectId)
-
+  def apply(projectId: String): F[Quota] = super.get(wrappedAt, uri / projectId)
+  
   /**
     * Shows quota usage for a project.
     * Cinder always returns a Quota even if the project does not exist. That is why there is no method called `getUsage`.
     * @param projectId The UUID of the project.
     */
-  def applyUsage(projectId: String): F[QuotaUsage] = super.get(wrappedAt =
-    Some(name), (uri / projectId).withQueryParam("usage", true))
-
+  def applyUsage(projectId: String): F[QuotaUsage] = super.get(wrappedAt, (uri / projectId).withQueryParam("usage", true))
+  
   /**
     * Gets default quotas for a project.
     * Cinder always returns a Quota even if the project does not exist. That is why there is no method called `getDefaults`.
     * @param projectId The UUID of the project.
     */
-  def applyDefaults(projectId: String): F[Quota] = super.get(wrappedAt = Some(name), uri / projectId / "defaults")
-
+  def applyDefaults(projectId: String): F[Quota] = super.get(wrappedAt, uri / projectId / "defaults")
+  
   /**
     * Updates quotas for a project.
     * @param projectId The UUID of the project.
     */
   def update(projectId: String, quotas: Quota.Update)(implicit encoder: Encoder[Quota.Update]): F[Quota] =
-    super.put(wrappedAt = Some(name), quotas, uri / projectId)
-
+    super.put(wrappedAt, quotas, uri / projectId)
+  
   /**
     * Deletes quotas for a project so the quotas revert to default values.
     * @param projectId The UUID of the project.
--- src/main/scala/pt/tecnico/dsi/openstack/cinder/services/Volumes.scala
+++ src/main/scala/pt/tecnico/dsi/openstack/cinder/services/Volumes.scala
@@ -10,7 +10,7 @@ import pt.tecnico.dsi.openstack.keystone.models.Session
 
 final class Volumes[F[_]: Sync: Client](baseUri: Uri, session: Session)
   extends CrudService[F, Volume, Volume.Create, Volume.Update](baseUri, "volume", session.authToken) {
-
+  
   /**
     * Creates a new volume.
     *
@@ -18,27 +18,37 @@ final class Volumes[F[_]: Sync: Client](baseUri: Uri, session: Session)
     * @param volume the volume create options.
     */
   override def create(volume: Volume.Create, extraHeaders: Header*): F[Volume] = super.create(volume, extraHeaders:_*)
-
+  
+  /** This method throws a NotImplementedError. */
+  override def defaultResolveConflict(existing: Volume, create: Volume.Create, keepExistingElements: Boolean, extraHeaders: Seq[Header]): F[Volume] = ???
+  
+  /** Creating a Volume cannot be implemented idempotently. That is why this method throws a NotImplementedError. */
+  override def createOrUpdate(create: Volume.Create, keepExistingElements: Boolean, extraHeaders: Seq[Header])(resolveConflict: (Volume, Volume.Create) =>
+    F[Volume]): F[Volume] = {
+    // Should we throw inside IO?
+    ???
+  }
+  
   /**
     * Lists summary information for all Block Storage volumes that the project can access.
     *
     * @param query extra query params to pass in the request.
     */
   def listSummary(query: Query = Query.empty): F[List[VolumeSummary]] = super.list[VolumeSummary](pluralName, uri.copy(query = query))
-
+  
   /**
     * Streams summary information for all Block Storage volumes that the project can access.
     *
     * @param query extra query params to pass in the request.
     */
   def streamSummary(query: Query = Query.empty): Stream[F, VolumeSummary] = super.stream[VolumeSummary](pluralName, uri.copy(query = query))
-
+  
   override def list(query: Query, extraHeaders: Header*): F[List[Volume]] =
     super.list[Volume](pluralName, (uri / "detail").copy(query = query), extraHeaders:_*)
-
+  
   override def stream(query: Query, extraHeaders: Header*): fs2.Stream[F, Volume] =
     super.stream[Volume](pluralName, (uri / "detail").copy(query = query), extraHeaders:_*)
-
+  
   /**
     * Deletes a volume.
     *
@@ -48,14 +58,4 @@ final class Volumes[F[_]: Sync: Client](baseUri: Uri, session: Session)
     */
   def delete(id: String, cascade: Boolean = false, force: Boolean = false): F[Unit] =
     super.delete((uri / id).withQueryParam("cascade", cascade).withQueryParam("force", force))
-
-  /** This method throws a NotImplementedError. */
-  override def defaultResolveConflict(existing: Volume, create: Volume.Create, keepExistingElements: Boolean, extraHeaders: Seq[Header]): F[Volume] = ???
-
-  /** Creating a Volume cannot be implemented idempotently. That is why this method throws a NotImplementedError. */
-  override def createOrUpdate(create: Volume.Create, keepExistingElements: Boolean, extraHeaders: Seq[Header])(resolveConflict: (Volume, Volume.Create) =>
-    F[Volume]): F[Volume] = {
-    // Should we throw inside IO?
-    ???
-  }
 }
